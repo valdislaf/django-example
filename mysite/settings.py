802c7a2106de81300a28862a5f373a4b02b5fad0
Vladislav Berestenko <valdislaf@gmail.com>
2022-11-30 12:16:49 +0300

    commit  only example

--- mysite/settings.py
+++ mysite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 
-only example
+# only example
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-uqfa1yqu#d(p@8o+!lbsu5_^%%@t40%m05ru*22059u$v@$b#)'
 
